3b4ad70b868d924192e62238248981a7da5dd9ed
Dizion56 <tihomir516@gmail.com>
2024-04-28 17:19:12 +0300

    Initial commit

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import { PageTitle } from "@/components/PageTitle.jsx";
+
+const DashboardPage = () => {
+  return (
+    <div className="flex flex-col gap-5 w-full text-left;">
+      <PageTitle />
+    </div>
+  );
+};
+export default DashboardPage;
---
+++ components/PageTitle.jsx
@@ -0,0 +1,5 @@
+import { cn } from "@/lib/utils.js";
+
+export const PageTitle = () => {
+  return <h1 className={"text-2xl font-semibold"}>DashBoard</h1>;
+};
